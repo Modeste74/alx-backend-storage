63d8c8ec41aa7ef8422c0f01f269c23c4e6b90f4
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-14 19:36:30 +0000

    procedure that find the scores for student and computes average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,23 @@
+-- creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10,2); -- Assuming average score is decimal (adjust precision and scale as needed)
+
+    -- Calculate the average score for the user
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+
+    -- Update the user's average score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+END;
+//
+
+DELIMITER ;
