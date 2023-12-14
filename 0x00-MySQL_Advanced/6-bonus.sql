ad290d6d97e5c2e883d0d3445bb8041753a4bd5f
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-14 19:24:51 +0000

    Procedure creation in sql

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+-- creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+DELIMITER //
+
+CREATE PROCEDURE AddBonus (
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    DECLARE project_id INT;
+
+    -- Check if the project exists, if not, create it
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    
+    IF project_id IS NULL THEN
+        -- Project doesn't exist, create a new one
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
+    -- Add the correction for the student
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
+//
+
+DELIMITER ;
