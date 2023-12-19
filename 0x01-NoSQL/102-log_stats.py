ec4f4f7f66b47b565852fd71369f7f9b0c3dcfe6
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-19 16:21:51 +0300

    Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""define a function log_stats"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    # Connect to MongoDB
+    client = MongoClient("mongodb://localhost:27017")
+    db = client['logs']
+    collection = db['nginx']
+    
+    # Get the number of documents in the collection
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods count
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Count the number of status check
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+    # Top 10 most frequent IPs
+    print("IPs:")
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+if __name__ == "__main__":
+    log_stats()
