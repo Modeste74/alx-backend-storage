1ae7af5318e336ff306777bcdc444b71bb5cd0fb
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-21 01:17:33 +0300

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""defines a function get_page"""
+import requests
+import redis
+import time
+
+# Initialize Redis connection
+redis_client = redis.Redis()
+
+def get_page(url: str) -> str:
+    """Get the HTML content of a URL and cache it with tracking"""
+    # Key for tracking the URL access count
+    access_count_key = f"count:{url}"
+
+    # Increment access count for the URL
+    redis_client.incr(access_count_key)
+
+    # Retrieve HTML content of the URL
+    response = requests.get(url)
+    html_content = response.text
+
+    # Cache the HTML content with an expiration time of 10 seconds
+    cache_key = f"cache:{url}"
+    redis_client.setex(cache_key, 10, html_content)
+
+    return html_content
+
